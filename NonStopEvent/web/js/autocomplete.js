70030543091508631ce544699e759b275d4415a1
cclzxl <cheng.cheng.chg@gmail.com>
2016-05-06 12:16:08 +0200

    Revert "Revert "home page city search""
    This reverts commit 02a171848ff485ba4a4017d6e672e4e17cf81ede.

---
+++ NonStopEvent/web/js/autocomplete.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var autocomplete;
+var countryRestrict = {'country': 'fr'};
+function init(){
+ autocomplete = new google.maps.places.Autocomplete(
+      /** @type {!HTMLInputElement} */ (
+          document.getElementById('autocomplete')), {
+        types: ['(cities)'],
+        componentRestrictions: countryRestrict
+      });
+      
+      autocomplete.addListener('place_changed', onPlaceChanged);
+      
+       document.getElementById('country').addEventListener(
+      'change', setAutocompleteCountry);
+}
+      
+function onPlaceChanged() {
+  var place = autocomplete.getPlace();
+  if (place.geometry) {
+  } else {
+    document.getElementById('autocomplete').placeholder = 'Ville';
+  }
+}
+
+function setAutocompleteCountry() {
+  var country = document.getElementById('country').value;
+  autocomplete.setComponentRestrictions({'country': country});
+
+}
