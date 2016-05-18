ecac9818bd8fa3c85a3becde6154a18fc9f04453
Sami <sami@slt.fr>
2016-05-19 00:26:22 +0200

    debug instafeed

--- NonStopEvent/web/js/eventful.js
+++ NonStopEvent/web/js/eventful.js
@@ -66,6 +66,8 @@ function eventinfos()
         var eventCountry = oData.country;
         var eventStart = new Date(oData.start_time);
         var eventEnd = new Date(oData.stop_time);
+        var lat = oData.latitude;
+        var long = oData.longitude;
         var eventTags = oData.tags;
         var eventTagss = [];
         var i = 0;
@@ -120,9 +122,53 @@ map.setZoom(14);
   });
     
 
+
+        var reqfb = $.getJSON('https://api.instagram.com/v1/locations/search?lat=' + lat + '&lng=' + long + '&access_token=523407829.1677ed0.e4b8167878444ab79936d95eb6112d3e');
+
+        /*console.log(reqfb);
+
+        var res = reqfb.id[0];
+        console.log(reqfb);*/
+
+        var feed = new Instafeed({
+            get: 'location',
+            locationId: 273471170716,
+            clientId: 'b7699de0ea314370aeb5466a86505c85',
+            accessToken: '523407829.1677ed0.e4b8167878444ab79936d95eb6112d3e',
+            target: 'insta',
+            resolution: 'low_resolution',
+            limit: 24,
+            template: '<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 nopadding"><a href="{{link}}" target="_blank"><img src="{{image}}" class="img-responsive" /></a></div>'
+        });
+        feed.run();
+
+/*        function getMultipleTags(tags) {
+            var feeds = [];
+            for (var i = 0, len = tags.length; i < len; i++) {
+                feeds.push(new Instafeed({
+                    // rest of your options
+                    get: 'tagged',
+                    tagName: tags[i],
+                    clientId: 'b7699de0ea314370aeb5466a86505c85',
+                    accessToken: '523407829.1677ed0.e4b8167878444ab79936d95eb6112d3e',
+                    target: 'insta',
+                    resolution: 'low_resolution',
+                    limit: 4,
+                    template: '<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 nopadding"><a href="{{link}}" target="_blank"><img src="{{image}}" class="img-responsive" /></a></div>'
+                }));
+            }
+            return feeds;
+        }
+// get multiple tags
+        myTags = getMultipleTags(eventTagss);
+
+        console.log(eventTagss);
+// run each instance
+        for(var i=0, len=myTags.length; i < len; i++) {
+            console.log(myTags[i]);
+            myTags[i].run();
+        }*/
     });
-
-
 
 }
 
