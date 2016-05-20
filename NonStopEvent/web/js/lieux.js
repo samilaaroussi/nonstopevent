df3f67c630f11aaf54096f429cf74ad7f8f6a254
cclzxl <cheng.cheng.chg@gmail.com>
2016-05-20 23:57:25 +0200

    foursquare
    foursquare

--- NonStopEvent/web/js/lieux.js
+++ NonStopEvent/web/js/lieux.js
@@ -10,8 +10,13 @@ var venues = [];
 function foursquare() {
     $.getJSON('https://api.foursquare.com/v2/venues/search?ll=40.7,-74&query=' + QUERY + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&limit=10&v=20140806',
         function (data) {
-            venues = data;
-            console.log("a:" + data);
+            venues = data.response.venues;
+            for(var i=0; i<venues.length; i++)
+            {
+                console.log(venues[i].name);
+                console.log(venues[i].id);
+            }
+            
         });
 }
 
