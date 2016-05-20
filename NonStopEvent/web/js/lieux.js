a65571653b6063fabef4dd7bf5a2b94b049e85e3
Sami <sami@slt.fr>
2016-05-21 00:18:41 +0200

    fill in places by id

--- NonStopEvent/web/js/lieux.js
+++ NonStopEvent/web/js/lieux.js
@@ -1,23 +1,3 @@
-/***************** Foursquare API *****************/
 
-var CLIENT_ID = '0JLT1NODQQWLG0C3G5MLPS4GFQKCAK5GBRUI5CQC2W2MX23Q';
-var CLIENT_SECRET = 'REEBOMIXJ0RAZO0D4UTBZXLBBSE5FGNHG05MUTF5P4B1MIXE';
-var LATLON = '40.7,-74'; //Position de l'évènement
-var QUERY = 'hotel';
-var i = 0;
-var venues = [];
-
-function foursquare() {
-    $.getJSON('https://api.foursquare.com/v2/venues/search?ll=40.7,-74&query=' + QUERY + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&limit=10&v=20140806',
-        function (data) {
-            venues = data.response.venues;
-            for(var i=0; i<venues.length; i++)
-            {
-                console.log(venues[i].name);
-                console.log(venues[i].id);
-            }
-            
-        });
-}
 
 /***************** Foursquare API *****************/
