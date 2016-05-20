6cbf0a8efe720485f251ca67d545a19013157053
Sami <sami@slt.fr>
2016-05-21 00:22:06 +0200

    add "f" letter to foursquare ids

--- NonStopEvent/web/js/eventful.js
+++ NonStopEvent/web/js/eventful.js
@@ -18,11 +18,11 @@ function addPlaces() {
         $.getJSON('https://api.foursquare.com/v2/venues/search?ll=40.7,-74&query=' + QUERY + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&limit=10&v=20140806',
             function (data) {
                 venues = data.response.venues;
-                for(var i=0; i<4; i++)
+                for(var i=0; i<venues.length; i++)
                 {
                     console.log(venues[i].name);
                     console.log(venues[i].id);
-                    places[venues[i].id] = venues[i];
+                    places["f" + venues[i].id] = venues[i];
                 }
 
             });
