81ecc99c4915b10205ca12e00b88bbeafaa18215
Sami <sami@slt.fr>
2016-05-28 18:17:51 +0200

    showplace foursquare

--- NonStopEvent/web/js/eventful.js
+++ NonStopEvent/web/js/eventful.js
@@ -54,6 +54,7 @@ function addPlaces(type, radius) {
                                 places["f" + venue.id].photo = photo;
                                 console.log(photo);
                             }
+                            showPlace(venue.id);
                         }
                     );
                 }
