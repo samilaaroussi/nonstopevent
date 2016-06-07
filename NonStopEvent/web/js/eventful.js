385ea293c665255ef6a689e1c2cfff75a3c6df2b
cclzxl <cheng.cheng.chg@gmail.com>
2016-06-07 18:55:42 +0200

    google rating

--- NonStopEvent/web/js/eventful.js
+++ NonStopEvent/web/js/eventful.js
@@ -188,53 +188,6 @@ map.setZoom(14);
     position: myLatLng,
     map: map,
   });
-
-
-
-        var reqfb = $.getJSON('https://api.instagram.com/v1/locations/search?lat=' + lat + '&lng=' + long + '&access_token=523407829.1677ed0.e4b8167878444ab79936d95eb6112d3e');
-
-        /*console.log(reqfb);
-        var res = reqfb.id[0];
-        console.log(reqfb);*/
-
-        var feed = new Instafeed({
-            get: 'location',
-            locationId: 273471170716,
-            clientId: 'b7699de0ea314370aeb5466a86505c85',
-            accessToken: '523407829.1677ed0.e4b8167878444ab79936d95eb6112d3e',
-            target: 'insta',
-            resolution: 'low_resolution',
-            limit: 24,
-            template: '<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 nopadding"><a href="{{link}}" target="_blank"><img src="{{image}}" class="img-responsive" /></a></div>'
-        });
-        feed.run();
-
-/*        function getMultipleTags(tags) {
-            var feeds = [];
-            for (var i = 0, len = tags.length; i < len; i++) {
-                feeds.push(new Instafeed({
-                    // rest of your options
-                    get: 'tagged',
-                    tagName: tags[i],
-                    clientId: 'b7699de0ea314370aeb5466a86505c85',
-                    accessToken: '523407829.1677ed0.e4b8167878444ab79936d95eb6112d3e',
-                    target: 'insta',
-                    resolution: 'low_resolution',
-                    limit: 4,
-                    template: '<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 nopadding"><a href="{{link}}" target="_blank"><img src="{{image}}" class="img-responsive" /></a></div>'
-                }));
-            }
-            return feeds;
-        }
-// get multiple tags
-        myTags = getMultipleTags(eventTagss);
-
-        console.log(eventTagss);
-// run each instance
-        for(var i=0, len=myTags.length; i < len; i++) {
-            console.log(myTags[i]);
-            myTags[i].run();
-        }*/
     });
 
 }
@@ -312,10 +265,13 @@ function search(type, distance)
                              }else
                              {
                                  places['g' + result.place_id].rating = -1;  
+                                 places['g' + result.place_id].ratingColor = ratingBg(-1);
                              }
+                             showPlace('g' + result.place_id);
                         }
+                        
                     });
-                    showPlace(placeId);
+                    
                 }
             }
 
@@ -643,7 +599,7 @@ function Integration()
 
 function merge(place_g, place_f)
 {
-    console.log();
+    console.log("merge:" + places[place_g].name);
     //merge photo
     if(places[place_g].hasOwnProperty('photo') && places[place_g].photo.indexOf('imageNotFound')>-1)
     {
