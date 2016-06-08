313fc91291ea915b2342ee545700f8794af9e38f
cclzxl <cheng.cheng.chg@gmail.com>
2016-06-09 01:13:50 +0200

    fusion
    fusion

--- NonStopEvent/web/js/eventful.js
+++ NonStopEvent/web/js/eventful.js
@@ -238,7 +238,7 @@ function onSearchButton()
      $('#googlemap').removeClass('col-md-12');
     $('#googlemap').addClass('col-md-6');
     $('#googlemap').css( "position", "fixed" );
-<<<<<<< HEAD
+
     //re-center google map
     google.maps.event.trigger(map, "resize");
     var descDiv = document.getElementById('desc');
@@ -249,10 +249,8 @@ function onSearchButton()
     var myLatLng = new google.maps.LatLng(parseFloat(latitude),parseFloat(longitude));
     map.setCenter(myLatLng);
     map.setZoom(14);
-   
-=======
-    google.maps.event.trigger(map, 'resize');
->>>>>>> origin/master
+
+
     var type = document.getElementById('type').value;
     var radius = 1000 * parseInt(document.getElementById('radius').value);
 
